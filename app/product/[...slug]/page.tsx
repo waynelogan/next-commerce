0c9464b9ef246558f1f87a49f15cd73f8b9b2243
waynelogan <loganamollo@gmail.com>
2024-01-13 11:48:15 +0300

    finished product page

--- app/product/[...slug]/page.tsx
+++ app/product/[...slug]/page.tsx
@@ -2,7 +2,7 @@ import ImageGallery from '@/app/components/ImageGallery'
 import { fullProduct } from '@/app/interface'
 import { client } from '@/app/lib/sanity'
 import { Button } from '@/components/ui/button'
-import { Star } from 'lucide-react'
+import { Star, Truck } from 'lucide-react'
 
 
 async function getData(slug: string) {
@@ -23,22 +23,22 @@ async function getData(slug: string) {
 
 
 export default async function Product(
-    { params: { slug } } : { params: { slug: string }}
+    { params: { slug } }: { params: { slug: string } }
 ) {
     const data: fullProduct = await getData(slug)
 
     return (
         <div className='bg-white'>
-            <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+            <div className="mx-auto w-fill px-4 md:px-8">
                 <div className="grid gap-8 md:grid-cols-2">
                     <ImageGallery images={data.images} />
                     <div className='md:py-8'>
                         <div className='mb-2 md:mb-3'>
                             <span className='mb-0.5 inline-block text-gray-500'>
-                                { data.categoryName }
+                                {data.categoryName}
                             </span>
                             <h2 className='text-2xl font-bold text-gray-800 lg:text-3xl'>
-                                { data.name }
+                                {data.name}
                             </h2>
                         </div>
                         <div className='mb-6 flex items-center gap-3 md:mb-10'>
@@ -56,10 +56,30 @@ export default async function Product(
                                     KES {data.price.toLocaleString('en-US')}
                                 </span>
                                 <span className=' mb-0.5 text-red-500 line-through'>
-                                    KES {(data.price * (1/0.7)).toFixed(0).toLocaleString('en-US') }
+                                    KES {Number((data.price * (1 / 0.7)).toFixed(0)).toLocaleString('en-US')}
                                 </span>
                             </div>
+                            <span className='text-sm text-gray-500'>
+                                Incl. VAT plus shipping
+                            </span>
                         </div>
+                        <div className='mb-6 flex items-center gap-2 text-gray-500'>
+                            <Truck className='w-6 h-6' />
+                            <span className='text-sm'>
+                                2-4 Day Shipping
+                            </span>
+                        </div>
+                        <div className='flex gap-2.5'>
+                            <Button>
+                                Add To Bag
+                            </Button>
+                            <Button variant={'secondary'}>
+                                Checkout Now
+                            </Button>
+                        </div>
+                        <p className='mt-12 text-base text-gray-500 tracking-wide'>
+                            {data.description}
+                        </p>
                     </div>
                 </div>
             </div>
